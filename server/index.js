8a8ed0a12a0aac5145bbf9c3b9495e7e0a1e4ac0
gjhutty <gjhutty@wgu.edu>
2025-05-07 14:26:51 -0600

    Jank ah Cors Options thought i'd try it at least

--- server/index.js
+++ server/index.js
@@ -14,11 +14,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin: 'https://gursavakhjhutty.github.io',
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
-app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
